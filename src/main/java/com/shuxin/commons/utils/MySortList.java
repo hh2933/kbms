d75939b23afd611890879cc0c00ca61f14ef2d5f
zengjiaheng <zengjiaheng@live.com>
2018-07-25 16:42:03 +0800

    0

--- src/main/java/com/shuxin/commons/utils/MySortList.java
+++ src/main/java/com/shuxin/commons/utils/MySortList.java
@@ -15,7 +15,7 @@ import java.util.List;
  * @param <E>
  */
 public class MySortList<E> {
-    
+
     /**
      * 对列表中的数据按指定字段进行排序。要求类必须有相关的方法返回字符串、整型、日期等值以进行比较。
      * @param list
@@ -23,16 +23,16 @@ public class MySortList<E> {
      * @param reverseFlag
      */
     public void sortByMethod(List<E> list, final String method,
-            final boolean reverseFlag) {
+                             final boolean reverseFlag) {
         Collections.sort(list, new Comparator<Object>() {
             @SuppressWarnings("unchecked")
             public int compare(Object arg1, Object arg2) {
                 int result = 0;
                 try {
-                    Method m1 = ((E) arg1).getClass().getMethod(method, null);
-                    Method m2 = ((E) arg2).getClass().getMethod(method, null);
-                    Object obj1 = m1.invoke(((E)arg1), null);
-                    Object obj2 = m2.invoke(((E)arg2), null);
+                    Method m1 = ((E) arg1).getClass().getMethod(method, new Class[0]);
+                    Method m2 = ((E) arg2).getClass().getMethod(method, new Class[0]);
+                    Object obj1 = m1.invoke(((E)arg1), new Object[]{});
+                    Object obj2 = m2.invoke(((E)arg2), new Object[]{});
                     if(obj1 instanceof String) {
                         // 字符串
                         result = obj1.toString().compareTo(obj2.toString());
@@ -52,10 +52,10 @@ public class MySortList<E> {
                     }else {
                         // 目前尚不支持的对象，直接转换为String，然后比较，后果未知
                         result = obj1.toString().compareTo(obj2.toString());
-                        
+
                         System.err.println("MySortList.sortByMethod方法接受到不可识别的对象类型，转换为字符串后比较返回...");
                     }
-                    
+
                     if (reverseFlag) {
                         // 倒序
                         result = -result;
